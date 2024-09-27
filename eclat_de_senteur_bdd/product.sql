c21b459f29d9ed119e784c02d676f6d329ed336a
CARLITTA <carla.elatre@gmail.com>
2024-09-27 16:44:49 +0200

    CHANGE schema products add propertie img

--- eclat_de_senteur_bdd/product.sql
+++ eclat_de_senteur_bdd/product.sql
@@ -5,7 +5,8 @@ CREATE TABLE product (
     id INT AUTO_INCREMENT PRIMARY KEY,      -- Identifiant unique pour chaque produit
     name VARCHAR(100) NOT NULL,              -- Nom du produit
     price DECIMAL(10, 2) NOT NULL,           -- Prix du produit
-    description TEXT NOT NULL,                -- Description du produit
+    description TEXT NOT NULL,  
+    img VARCHAR(255) DEFAULT NULL,              -- Description du produit
     stock INT NOT NULL,                       
     categorie_id INT NOT NULL,                -- Référence à la catégorie du produit
     CONSTRAINT `fk_categorie_product` FOREIGN KEY(categorie_id) REFERENCES categorie(id) -- Clé étrangère vers Categorie
